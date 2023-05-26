b75f631476892969d5adec10b7bdef5784ebcfbe
Hurley2017 <luciefer9062hurley@gmail.com>
2023-05-26 21:57:02 +0530

    gpa final or wut?

--- main.py
+++ main.py
@@ -1,28 +1,4 @@
-from flask import Flask, request
-from bardapi import Bard
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-api = Flask(__name__)
-
-@api.route('/', methods=['GET'])
-def heartbeat():
-    return "Yep! Alive..."
-
-
-@api.route('/getBard_Response', methods=['POST'])
-def returnB():
-    var = {'Content' : "bhag bsdk"}
-    API_key = os.environ.get('API_KEY')
-    ai = Bard(token=API_key)
-    Package = request.json
-    if Package['Key'] == os.environ.get('USR_KEY'):
-        test = ai.get_answer(Package['Query'])
-        var["Content"]=test['content']  
-    return var
-
-if "__main__" == __name__:
-    api.run()
+from server import app
+risk_hai = app
+if __name__ == "__main__":
+    risk_hai.run()
---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from bardapi import Bard
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def heartbeat():
+    return "Yep! Alive..."
+
+@app.route('/getBard_Response', methods=['POST'])
+def returnB():
+    var = {'Content' : "bhag bsdk"}
+    API_key = os.environ.get('API_KEY')
+    ai = Bard(token=API_key)
+    Package = request.json
+    if Package['Key'] == os.environ.get('USR_KEY'):
+        test = ai.get_answer(Package['Query'])
+        var["Content"]=test['content']  
+    return var
+
+if "__main__" == __name__:
+    app.run()
