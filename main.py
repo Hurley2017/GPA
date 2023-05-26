55244f0305b2b662e3cb4b8443babb7b21066153
Hurley2017 <luciefer9062hurley@gmail.com>
2023-05-26 21:36:20 +0530

    lets see

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from server import api
+from flask import Flask, request
+from bardapi import Bard
+import os
+from dotenv import load_dotenv
 
-Object = api
+load_dotenv()
+
+
+api = Flask(__name__)
+
+@api.route('/', methods=['GET'])
+def heartbeat():
+    return "Yep! Alive..."
+
+
+@api.route('/getBard_Response', methods=['POST'])
+def returnB():
+    var = {'Content' : "bhag bsdk"}
+    API_key = os.environ.get('API_KEY')
+    ai = Bard(token=API_key)
+    Package = request.json
+    if Package['Key'] == os.environ.get('USR_KEY'):
+        test = ai.get_answer(Package['Query'])
+        var ["Content"]=test['content']  
+    return var
 
 if "__main__" == __name__:
-    Object.run(debug=True)
+    api.run()
