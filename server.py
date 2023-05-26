e44fa6c536361d2147515d8e0f2a9e95b8654bd8
Hurley2017 <luciefer9062hurley@gmail.com>
2023-05-26 21:33:46 +0530

    ektu try

--- server.py
+++ server.py
@@ -13,14 +13,14 @@ def heartbeat():
     return "Yep! Alive..."
 
 
-@api.route('/getBard_Response', methods=['POST'])
-def returnB():
-    var = {'Content' : "bhag bsdk"}
-    API_key = os.environ.get('API_KEY')
-    ai = Bard(token=API_key)
-    Package = request.json
-    if Package['Key'] == os.environ.get('USR_KEY'):
-        test = ai.get_answer(Package['Query'])
-        var ["Content"]=test['content']  
-    return var
+# @api.route('/getBard_Response', methods=['POST'])
+# def returnB():
+#     var = {'Content' : "bhag bsdk"}
+#     API_key = os.environ.get('API_KEY')
+#     ai = Bard(token=API_key)
+#     Package = request.json
+#     if Package['Key'] == os.environ.get('USR_KEY'):
+#         test = ai.get_answer(Package['Query'])
+#         var ["Content"]=test['content']  
+#     return var
 
